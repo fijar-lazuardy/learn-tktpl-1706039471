b6d26e034301f4e1ac385d7538bc3e73b1e01208
Fijar Lazuardy <fijarlaz@gmail.com>
2020-10-10 23:29:56 +0700

    add base activity

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/fijar/tktpl/lab_4/Lab4Activity.kt
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.mobileprogramming.fijar.tktpl.lab_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ui.cs.mobileprogramming.fijar.tktpl.R
+
+class Lab4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.lab_4)
+    }
+}
